728861d730ca6657c1b331209de662cd59254bd8
Ihuoma Ndukwe <chineme4g@gmail.com>
2022-02-26 17:33:48 +0100

    finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
           <img src= {profile} height={317} width={317}/>
       <div className="head-text"><h1>Ndukwe Ihuoma</h1>
       <h3>Frontend Developer</h3>
-      <p>ihuoma.website</p></div>
+      <a href='https://ihuoma5.netlify.app/'><p>ihuoma.website</p></a></div>
       <div className="btn">
           <button className="email">Email</button>
           <button className="linkedin">LinkedIn</button>
